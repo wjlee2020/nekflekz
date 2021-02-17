562b69341b8c41f0f53f2a5991e17c622e644213
WJ Lee <wjlee2122@gmail.com>
2021-02-17 11:19:22 +0900

    styling the profile component
    the profile component styling (which is in the browse page. This profile component only shows when profile.displayName is null (when the profile is not selected) as the click will setProfile object to the clicked profile.

--- src/components/profiles/styles/profiles.js
+++ src/components/profiles/styles/profiles.js
@@ -5,23 +5,56 @@ export const Container = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    margin: auto;
+    max-width: 80%;
 `;
 
-export const Title = styled.h1``;
+export const Title = styled.h1`
+    width: 100%;
+    color: #fff;
+    font-size: 48px;
+    text-align: center;
+    font-weight: 500;
+`;
 
-export const List = styled.ul``;
+export const List = styled.ul`
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-direction: row;
 
-export const Name = styled.p``;
+`;
+
+export const Name = styled.p`
+    color: #808080;
+    text-overflow: ellipsis;
+    font-size: 16px;
+    cursor: pointer;
+`;
 
 export const Picture = styled.img`
-    height: 10vw;
-    width: 10vw;
-    max-height: 200px;
-    max-width: 200px;
-    min-height: 84px;
-    min-width: 84px;
+    height: auto;
+    width: 100%;
+    max-width: 150px;
+    border: 3px solid black;
+    cursor: pointer;
 `;
 
 export const Item = styled.li`
     list-style-type: none;
+    max-height: 200px;
+    max-width: 200px;
+    text-align: center;
+    margin-right: 30px;
+
+    &:hover > ${Picture} {
+        border: 3px solid white;
+    }
+    &:hover > ${Name} {
+        font-weight: bold;
+        color: white;
+    }
+    &:last-of-type {
+        margin-right: 0;
+    }
 `;
